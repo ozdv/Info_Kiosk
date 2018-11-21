3b1a1e115b2b185f9ea338208e70a90b19fc9354
xsmsx <xsm.exe@gmail.com>
2018-11-21 00:06:04 -0700

    added home page with icons

--- WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
+++ WpfApp1/obj/Debug/WpfApp1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/back_icon.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("assets/govdoc.png")]
 
 
